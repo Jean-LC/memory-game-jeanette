d30812038269250dd8079efe742d67993b60507e
Jeanette Latisnere Carrillo <jlatisnere@tiprotec.com>
2026-05-13 19:15:59 -0600

    Adds useAudio hook to play audio

---
+++ src/hooks/useAudio.ts
@@ -0,0 +1,22 @@
+import { useRef } from "react";
+
+export const useAudio = () => {
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  const play = () => {
+    if (audioRef.current) {
+      audioRef.current.volume = 0.3;
+      audioRef.current.play();
+    }
+  };
+
+  const pause = () => {
+    audioRef.current?.pause();
+  };
+
+  return {
+    audioRef,
+    play,
+    pause,
+  };
+};
