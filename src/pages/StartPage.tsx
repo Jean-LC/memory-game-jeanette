8ea7a90e205b6505f972f5feefe10d2561e7f6fa
Jeanette Latisnere Carrillo <jlatisnere@tiprotec.com>
2026-05-13 19:27:52 -0600

    Adds animation to start page elements

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,22 +1,36 @@
 import { Layout } from "../components/Layout";
 import logo from "../assets/images/logo.svg";
 import { useCardsStore } from "../store/cardsStore";
+import { useState, useEffect } from "react";
 
 export const StartPage = () => {
   const { setPagination } = useCardsStore();
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setVisible(true), 50);
+  }, []);
   return (
     <Layout>
       <div className="flex flex-col justify-center items-center w-full h-full ">
+        {/* <h1
+          className={`
+        transition-all duration-700 ease-out
+        ${visible ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-10"}
+      `}
+        >
+          Mi Título
+        </h1> */}
         <img
           src={logo}
           alt="logo"
-          className="w-1/2 md:w-1/4 h-auto object-contain"
+          className={`w-1/2 md:w-1/4 h-auto object-contain transition-all duration-700 ease-out ${visible ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-20"}`}
         />
-        <p className="text-2xl text-violet-500 my-8 text-center">
+        <p className="text-2xl text-blue-800 my-8 text-center">
           Welcome to the memory game!
         </p>
         <button
-          className="w-16 h-8 bg-violet-400 text-white rounded-md "
+          className={`w-2xs h-8 bg-yellow-500 text-white rounded-md transition-all duration-700 ease-out ${visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-20"} hover:animate-bounce`}
           onClick={() => setPagination("game")}
         >
           Start
