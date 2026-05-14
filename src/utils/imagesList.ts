aefd6d09fb8b96287b5254ed6f110681a27ff06c
Jeanette Latisnere Carrillo <jlatisnere@tiprotec.com>
2026-05-14 12:38:34 -0600

    adds image list to utils folder

---
+++ src/utils/imagesList.ts
@@ -0,0 +1,23 @@
+import comet from "../assets/images/comet.svg";
+import moon from "../assets/images/moon.svg";
+import star from "../assets/images/star.svg";
+import sun from "../assets/images/sun.svg";
+
+export const imagesList = [
+  {
+    id: 1,
+    src: comet,
+  },
+  {
+    id: 2,
+    src: moon,
+  },
+  {
+    id: 3,
+    src: star,
+  },
+  {
+    id: 4,
+    src: sun,
+  },
+];
