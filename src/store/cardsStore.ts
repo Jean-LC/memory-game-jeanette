835a3611d951d224114d21d3c65466b2994f6087
Jeanette Latisnere Carrillo <jlatisnere@tiprotec.com>
2026-05-13 18:47:41 -0600

    Adds store for the app

---
+++ src/store/cardsStore.ts
@@ -0,0 +1,36 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface IData {
+  pagination: "start" | "game" | "finish";
+  userWin: boolean;
+  userTime: number;
+}
+
+interface IActions {
+  setPagination: (pagination: "start" | "game" | "finish") => void;
+  setUserWin: (userWin: boolean) => void;
+  setUserTime: (userTime: number) => void;
+}
+
+export const useCardsStore = create<IData & IActions>()(
+  persist(
+    (set) => ({
+      pagination: "finish",
+      setPagination: (pagination: "start" | "game" | "finish") =>
+        set({ pagination }),
+
+      userWin: false,
+      setUserWin: (userWin) => set({ userWin }),
+
+      userTime: 0,
+      setUserTime: (userTime) => set({ userTime }),
+    }),
+    {
+      name: "cards-storage",
+      partialize: (state) => ({
+        // pagination: state.pagination,
+      }),
+    },
+  ),
+);
