1f888c678ec16954e5238ee9359450ba2c487129
Jeanette Latisnere Carrillo <jlatisnere@tiprotec.com>
2026-05-14 12:38:51 -0600

    updates finish page with best times

--- src/pages/FinishPage.tsx
+++ src/pages/FinishPage.tsx
@@ -1,13 +1,37 @@
+import { BestTimeTable } from "../components/BestTimeTable";
 import { Layout } from "../components/Layout";
 import { useCardsStore } from "../store/cardsStore";
+import logo from "../assets/images/logo.svg";
+import { EnterNameModal } from "../components/EnterNameModal";
+import { useEffect, useState } from "react";
 
 export const FinishPage = () => {
-  const { setPagination, userWin } = useCardsStore();
+  const { setPagination, userWin, userTime, bestTimes } = useCardsStore();
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  function validateTime() {
+    const existingTime = bestTimes.find((record) => record.time === userTime);
+    const allTimes = [...bestTimes.map((record) => record.time), userTime]
+      .sort((a, b) => a - b)
+      .slice(0, 3);
+
+    if (!existingTime && allTimes.includes(userTime)) setIsOpenModal(true);
+  }
+
+  useEffect(() => {
+    validateTime();
+  }, []);
+
   return (
     <Layout>
       <div className="flex flex-col justify-center items-center w-full h-full ">
+        <img
+          src={logo}
+          alt="logo"
+          className={"w-1/4 md:w-1/12 h-auto object-contain"}
+        />
         <p
-          className={`text-4xl ${userWin ? "text-green-700" : "text-red-700"} my-16 text-center`}
+          className={` text-2xl md:text-4xl ${userWin ? "text-green-700" : "text-red-700"} my-4 md:my-16 text-center`}
         >
           {userWin ? "You did it!" : "oops you didn't find them all"}
         </p>
@@ -26,6 +50,11 @@ export const FinishPage = () => {
           </button>
         </div>
       </div>
+      <BestTimeTable />
+      <EnterNameModal
+        isOpen={isOpenModal}
+        close={() => setIsOpenModal(false)}
+      />
     </Layout>
   );
 };
